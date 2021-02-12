3c36ee993c20575a0a75142e6c74246545dc1561
Egstad <jordanegstad@gmail.com>
2021-02-12 00:16:38 -0800

    housekeeping

--- schemas/documents/page.js
+++ schemas/documents/page.js
@@ -1,6 +1,7 @@
 import { MdDescription } from 'react-icons/md'
 import { slugIsUnique } from '../../lib/slugIsUnique'
 import { slugify } from '../../lib/slugify'
+import Tabs from 'sanity-plugin-tabs'
 
 export default {
   name: 'page',
@@ -9,40 +10,73 @@ export default {
   icon: MdDescription,
   fields: [
     {
-      name: 'title',
-      title: 'Page Title',
-      type: 'string',
-      description: 'Shows up in title of page',
-      validation: (Rule) => Rule.required(),
-    },
-    {
-      title: 'Slug',
-      name: 'slug',
-      type: 'slug',
-      description: 'for url',
-      options: {
-        source: 'title',
-        isUnique: slugIsUnique,
-        slugify: slugify,
-      },
-      validation: (Rule) => Rule.required(),
-    },
-    {
-      title: 'Tags',
-      name: 'tags',
-      type: 'array',
-      of: [
+      name: 'tabs',
+      type: 'object',
+      inputComponent: Tabs,
+      fieldsets: [
+        { name: 'info', title: 'Info', options: { sortOrder: 10 } },
+        { name: 'content', title: 'Content', options: { sortOrder: 20 } },
+        { name: 'seo', title: 'SEO', options: { sortOrder: 30 } },
+      ],
+      fields: [
         {
-          type: 'reference',
-          to: [{ type: 'tag' }],
+          name: 'title',
+          title: 'Page Title',
+          type: 'string',
+          description: 'Shows up in title of page',
+          fieldset: 'info',
+          validation: (Rule) => Rule.required(),
+        },
+        {
+          title: 'Slug',
+          name: 'slug',
+          type: 'slug',
+          description: 'for url',
+          fieldset: 'info',
+          options: {
+            source: (doc, options) => {
+              return doc.tabs.title
+            },
+            isUnique: slugIsUnique,
+            slugify: slugify,
+          },
+          validation: (Rule) => Rule.required(),
+        },
+        {
+          title: 'Tags',
+          name: 'tags',
+          type: 'array',
+          fieldset: 'info',
+          of: [
+            {
+              type: 'reference',
+              to: [{ type: 'tag' }],
+            },
+          ],
+        },
+        {
+          name: 'slices',
+          type: 'array',
+          title: 'Content',
+          description: 'Add a picture, video, text, w/e tf!',
+          fieldset: 'content',
+          of: [{ type: 'picture' }, { type: 'video' }, { type: 'richText' }],
         },
       ],
     },
-    {
-      name: 'slices',
-      type: 'array',
-      title: 'Page builder',
-      of: [{ type: 'picture' }, { type: 'video' }, { type: 'richText' }],
-    },
   ],
+
+  preview: {
+    select: {
+      title: 'tabs.title',
+      author: 'tabs.author.name',
+      media: 'tabs.mainImage',
+    },
+    prepare(selection) {
+      const { author } = selection
+      return Object.assign({}, selection, {
+        subtitle: author && `by ${author}`,
+      })
+    },
+  },
 }
