0e14aa1ca4070fc36596c54f170df086cc3d571f
Jordan Egstad <jordanegstad@gmail.com>
2021-02-21 22:30:13 -0800

    fix: typo

--- schemas/slices/video.js
+++ schemas/slices/video.js
@@ -1,36 +1,81 @@
-import videoEgstadCdn from '../../lib/videoEgstadCdn'
+import videoEgstadCdn from "../../lib/videoEgstadCdn";
 
 export default {
-  name: 'video',
-  type: 'object',
-  title: 'Video',
-  description: 'A video',
+  name: "video",
+  type: "object",
+  title: "Video",
+  description: "A video",
   fields: [
     {
-      name: 'url',
-      description: 'Paste the S3 object key here',
-      type: 'string',
-      title: 'Default Video',
-      validation: (Rule) => Rule.error('Default Video field is required!').required(),
-      inputComponent: videoEgstadCdn
+      name: "url",
+      description: "Paste the S3 object key here",
+      type: "string",
+      title: "Default Video",
+      validation: (Rule) =>
+        Rule.error("Default Video field is required!").required(),
+      inputComponent: videoEgstadCdn,
     },
     {
-      name: 'alt',
-      type: 'string',
-      title: 'Alternative text',
+      name: "urlMobile",
+      description:
+        "Optional video for small devices. Paste the S3 object key here",
+      type: "string",
+      title: "Small Video",
+      inputComponent: videoEgstadCdn,
+    },
+    {
+      name: "alt",
+      type: "string",
+      title: "Alternative text",
       validation: (Rule) =>
-        Rule.error('You have to fill out the alternative text.').required(),
-      description: 'Describe image for those using screen readers. Important for SEO and accessiblity.',
+        Rule.error("You have to fill out the alternative text.").required(),
+      description:
+        "Describe image for those using screen readers. Important for SEO and accessiblity.",
       options: {
         isHighlighted: true,
       },
     },
     {
-      name: 'urlMobile',
-      description: 'Optional video for small devices. Paste the S3 object key here',
-      type: 'string',
-      title: 'Small Video',
-      inputComponent: videoEgstadCdn
+      title: "Playback Settings",
+      name: "settings",
+      type: "object",
+      fields: [
+        {
+          name: "autoplay",
+          type: "boolean",
+          title: "Autoplay",
+          description: "Play the video right away. (Default: false)",
+        },
+        {
+          name: "loop",
+          type: "boolean",
+          title: "Loop",
+          description: "Loop the video when playback ends. (Default: false)",
+        },
+        {
+          name: "mute",
+          type: "boolean",
+          title: "Mute",
+          description: "Mute the audio during video playback. (Default: false)",
+        },
+        {
+          name: "controls",
+          type: "boolean",
+          title: "Controls",
+          description: "Show browser default controls or not. (Default: false)",
+        },
+        {
+          name: "playsinline",
+          type: "boolean",
+          title: "Playback Position (Playsinline)",
+          description:
+            "Play the video right where it is. Don't fullscreen on mobile by default. (Default: true)",
+        },
+      ],
     },
   ],
-}
+};
+
+// Mute
+// Autoplay
+// Loop
